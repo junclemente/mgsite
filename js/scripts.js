f916742a614c5b724c32cb6647daea0dadd4b1c7
Jun Clemente <kjstudios858@gmail.com>
2017-01-11 19:17:02 -0800

    add new marker

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,8 @@ function initMap() {
   var myMarkers = [
                     ['Admiral Kidd Conference and Catering Center', 32.722775, -117.217717],
                     ['Homewood Suites by Hilton San Diego', 32.729500, -117.215923],
-                    ['Courtyard San Diego Airport/Liberty Station', 32.730704, -117.215709]
+                    ['Courtyard San Diego Airport/Liberty Station', 32.730704, -117.215709],
+                    ['Holiday Inn Bayside', 32.726246, -117.222454]
                   ]
 
   // Assign google map to var map and add map to webpage to #map
