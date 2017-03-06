fb47c71a2e20a6287a2350aab9b181748878028a
Jun Clemente <junclemente@outlook.com>
2017-03-06 02:06:45 -0800

    working on adding countdown timer to knockout app

--- js/app.js
+++ js/app.js
@@ -33,6 +33,38 @@ var navBarList = [
 	}
 ];
 
+var CountDownModel = function(dt, id) {
+        var end = new Date(dt);
+
+        var _second = 1000;
+        var _minute = _second * 60;
+        var _hour = _minute * 60;
+        var _day = _hour * 24;
+        var timer;
+
+        function showRemaining() {
+            var now = new Date();
+            var distance = end - now;
+            if (distance < 0) {
+
+                clearInterval(timer);
+                document.getElementById(id).innerHTML = 'EXPIRED!';
+
+                return;
+            }
+            var days = Math.floor(distance / _day);
+            var hours = Math.floor((distance % _day) / _hour);
+            var minutes = Math.floor((distance % _hour) / _minute);
+            var seconds = Math.floor((distance % _minute) / _second);
+
+            document.getElementById(id).innerHTML = days + ' days  ';
+            document.getElementById(id).innerHTML += hours + ' hrs ';
+            document.getElementById(id).innerHTML += minutes + ' min ';
+            document.getElementById(id).innerHTML += seconds + ' sec';
+        }
+
+        timer = setInterval(showRemaining, 1000);
+};
 
 var NavBarModel = function(data) {
 
@@ -46,6 +78,7 @@ var AppViewModel = function() {
 
 	var that = this;
 	that.navigationList = ko.observableArray([]);
+	that.countdownClock = ko.observable(new CountDownModel('09/02/2017 12:00 AM', 'countdown'));
 
 	navBarList.forEach(function(data) {
 		that.navigationList.push(new NavBarModel(data));
@@ -60,6 +93,8 @@ var AppViewModel = function() {
 		if( $('.navbar-toggle').css('display') != 'none') {
 			$('.navbar-toggle').click();
 		}
+
+		location.hash = nav.url;
 	};
 };
 
--- js/scripts.js
+++ js/scripts.js
@@ -71,7 +71,7 @@ function CountDownTimer(dt, id)
             if (distance < 0) {
 
                 clearInterval(timer);
-                document.getElementById(id).innerHTML = 'EXPIRED!';
+                // document.getElementById(id).innerHTML = 'EXPIRED!';
 
                 return;
             }
