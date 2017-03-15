5eb5a0948ada0610263d19685c88983475aae0ef
Jun Clemente <junclemente@outlook.com>
2017-03-15 16:46:00 -0700

    refactor: Created new KO Model for General Wedding Info

--- js/app.js
+++ js/app.js
@@ -36,6 +36,45 @@ var CountDownModel = function() {
 	timer = setInterval(showRemaining, 1000);
 };
 
+var HotelListModel = function(hotel) {
+
+	var self = this;
+
+	self.name = hotel.name;
+	self.address = hotel.address;
+	self.city_state = hotel.city_state;
+	self.phone = 'tel:' + hotel.phone;
+	self.display_phone = hotel.display_phone;
+	self.booking_url = hotel.booking_url;
+	self.group_name = hotel.group_name;
+	self.group_code = hotel.group_code;
+	self.image_lg = "images/" + hotel.image_base + "-lg.jpg";
+	self.image_sm = "images/" + hotel.image_base + "-sm.jpg";
+	self.price_html = hotel.price_html;
+};
+
+var GeneralInfo = function() {
+
+	var self = this;
+
+	// General Information
+	self.wedding_date = 'September 02, 2017';
+	self.wedding_time = '5:00pm';
+	self.phone = 'tel:18588489060';
+	self.display_phone = '858.848.9060';
+	self.email_to = 'mailto:graceandmelvin@gmail.com';
+	self.display_email = 'graceandmelvin@gmail.com';
+	self.honeyfund_link = 'https://www.honeyfund.com/wedding/gracevin2017';
+	self.instagram_text = '#####';
+
+	// Shuttle Information
+	self.shuttle_roundtrip_start = '4:15pm';
+	self.shuttle_roundtrip_end = '4:55pm';
+	self.holiday_inn_start = '4:00pm';
+	self.holiday_inn_stop = '5:05pm';
+
+};
+
 var NavBarModel = function(data) {
 
 	var self = this;
@@ -47,12 +86,18 @@ var NavBarModel = function(data) {
 var AppViewModel = function() {
 
 	var that = this;
+	that.weddingInfo = ko.observable(new GeneralInfo());
 	that.navigationList = ko.observableArray([]);
+	that.hotelList = ko.observableArray([]);
 	that.countdownClock = ko.observable(new CountDownModel());
 	console.log(that.countdownClock().countdownValue());
 
 	NAVBAR_LIST.forEach(function(data) {
 		that.navigationList.push(new NavBarModel(data));
+	});
+
+	HOTEL_LIST.forEach(function(hotel) {
+		that.hotelList.push(new HotelListModel(hotel));
 	});
 
 	that.selectNavigation = function(nav) {
--- js/locations.js
+++ js/locations.js
@@ -32,3 +32,48 @@ var NAVBAR_LIST = [
 		url: "rsvp"
 	}
 ];
+
+var HOTEL_LIST = [
+	{
+		name: "Homewood Suites Liberty Station",
+		address: "2576 Laning Rd.",
+		city_state: "San Diego, CA 92016",
+		phone: "16192220500",
+		display_phone: "619.222.0500",
+		booking_url: "https://secure3.hilton.com/en_US/hw/reservation/book.htm?inputModule=HOTEL&ctyhocn=SANLSHW&spec_plan=CHWJCW&arrival=20170901&departure=20170903&cid=OM,WW,HILTONLINK,EN,DirectLink&fromId=HILTONLINKDIRECT",
+		group_name: "JANG CLEMENTE WEDDING",
+		group_code: "JCW",
+		image_base: "homewoodsuites",
+		price_html: 'Studio: $199/night<br>' +
+					'1-Bedroom Suite: $209/night<br>' +
+					'2-Bedroom Suite: $289/night<br>'
+	},
+	{
+		name: "Courtyard Marriot Liberty Station",
+		address: "2592 Laning Rd.",
+		city_state: "San Diego, CA 92016",
+		phone: "16192211900",
+		display_phone: "619.221.1900",
+		booking_url: "http://www.marriott.com/meeting-event-hotels/group-corporate-travel/groupCorp.mi?resLinkData=Jang%20Clemente%20Wedding%20%5Esanal%60JCWJCWA%7CJCWJCWB%60179%60USD%60false%601%609/1/17%609/3/17%6007/28/17&app=resvlink&stop_mobi=yes",
+		group_name: "JANG CLEMENTE WEDDING",
+		group_code: "JCW",
+		image_base: "courtyardmarriot",
+		price_html: '1-Bedroom: $179/night<br>'
+	},
+	{
+		name: "Holiday Inn San Diego Bayside",
+		address: "4875 North Harbor Drive",
+		city_state: "San Diego, CA 92016",
+		phone: "16192213061",
+		display_phone: "619.221.3061",
+		booking_url: "https://www.ihg.com/holidayinn/hotels/us/en/san-diego/sanby/hoteldetail?qAdlt=1&qBrs=6c.hi.ex.rs.ic.cp.in.sb.cw.cv.ul.vn.ki.sp.nd.ct&qChld=0&qFRA=1&qGRM=0&qGrpCd=CJW&qIta=99801505&qPSt=0&qRRSrt=rt&qRef=df&qRms=1&qRpn=1&qRpp=20&qSHp=1&qSmP=3&qSrt=sBR&qWch=0&srb_u=1&icdv=99801505",
+		group_name: "CLEMENTE-JANG WEDDING",
+		group_code: "",
+		image_base: "holidayinn",
+		price_html: 'Std Rm: $169/night<br>' +
+					'Executive Rm: $199/night<br>' +
+					'Suite: $269/night*<br>' +
+					'<p>* Suites cannot be booked online at the negotiated rate. ' +
+					'To book a suite, please call <a href="tel:18006628899">800.662.8899</a></p>'
+	}
+];
